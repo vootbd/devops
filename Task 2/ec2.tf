163284a4d4655892ff5643f171f38462178fc686
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-24 19:14:19 +0600

    day 3

--- Task 2/ec2.tf
+++ Task 2/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = 
-  profile   = 
+  region    = "us-west-2"
+  profile   = "bastionhost"
 }
 
 
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_default_vpc" "default_vpc" {
 
   tags    = {
-    Name  = 
+    Name  = "default vpc"
   }
 }
 
@@ -20,10 +20,10 @@ data "aws_availability_zones" "available_zones" {}
 
 # create default subnet if one does not exit
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = 
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
 
   tags   = {
-    Name = 
+    Name = "default subnet"
   }
 }
 
@@ -32,70 +32,94 @@ resource "aws_default_subnet" "default_az1" {
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22"
-  vpc_id      = 
+  vpc_id      = aws_default_vpc.default_vpc.id
 
+  # allow access on port 8080
   ingress {
     description      = "http access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
     description      = "ssh access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags   = {
-    Name = 
+    Name = "jenkins server security group"
   }
 }
 
 
 # use data source to get a registered amazon linux 2 ami
-data "aws_ami" "amazon_linux_2" {
+data "aws_ami" "ubuntu_22_04" {
   most_recent = true
-  owners      = ["amazon"]
-  
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-amd64-*"]  # Match Ubuntu 22.04 AMIs
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]  # Filter for HVM AMIs
   }
 }
-
 
 # launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance" {
-  ami                    = 
-  instance_type          = 
-  subnet_id              = 
-  vpc_security_group_ids = 
-  key_name               = 
-  user_data              = 
+  ami                    = data.aws_ami.ubuntu_22_04.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = "bastion_key"
+  #user_data              = 
 
   tags = {
-    Name = 
+    Name = "Jenkins server"
   }
 }
 
+# an empty resource block
+resource "null_resource" "name" {
 
-# print the ec2's public ipv4 address
-output "public_ipv4_address" {
-  value = 
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/bastion_key.pem")
+    host        = aws_instance.ec2_instance.public_ip
+  }
+
+  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+    source      = "jenkins_install.sh????"
+    destination = "/tmp/jenkins_install.sh"
+  }
+
+  # set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+    inline = [
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = []
+}
+
+
+# print the url of the jenkins server
+output "website_url" {
+  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
 }
