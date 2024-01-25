3a5ffd0c145ce943f73495669f38ac2a36cb6388
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-25 17:39:53 +0600

    update

--- Task 2/jenkins_install.sh
+++ Task 2/jenkins_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install openjdk-17-jre -y
+echo -e '\t'   # Tab
+echo -e '\n'   # Enter
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install jenkins -y
+echo -e '\t'   # Tab
+echo -e '\n'   # Enter
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
