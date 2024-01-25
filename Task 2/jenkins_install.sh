52182bfae2ec958791ed901b4f4e691116b62e88
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-25 17:26:53 +0600

    changed jenkins file

--- Task 2/jenkins_install.sh
+++ Task 2/jenkins_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo apt upgrade
-sudo dnf install java-17-amazon-corretto -y
+sudo sudo apt update -y && sudo apt upgrade -y
+sudo apt install openjdk-17-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo sudo apt update -y
 sudo apt install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
