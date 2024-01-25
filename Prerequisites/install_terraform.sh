00485845ca9dc521a2631c2834f5a20ed19b28a7
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-25 13:49:59 +0600

    terraform version check

--- Prerequisites/install_terraform.sh
+++ Prerequisites/install_terraform.sh
@@ -6,3 +6,4 @@ sudo apt install software-properties-common gnupg2 curl unzip wget -y
 #sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt install terraform
+terraform --version
