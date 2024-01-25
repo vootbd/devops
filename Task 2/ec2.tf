41a8c1d0c432e342db9c6f18893e08b3e1aaf63f
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-25 16:02:41 +0600

    provider added

--- Task 2/ec2.tf
+++ Task 2/ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-west-2"
