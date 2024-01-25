6f09205f5411b0204aa6274a7d513df95475b087
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-25 13:45:27 +0600

    commented 2 lines

--- Prerequisites/install_terraform.sh
+++ Prerequisites/install_terraform.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install  software-properties-common gnupg2 curl unzip wget
-curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
-sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+sudo apt install software-properties-common gnupg2 curl unzip wget -y
+#curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+#sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt install terraform
