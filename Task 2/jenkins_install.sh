544b691b31c8abf911126761a231d1847dc5c71e
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-28 11:53:23 +0600

    edited jenkins_install.sh file

--- Task 2/jenkins_install.sh
+++ Task 2/jenkins_install.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
+sudo sed -i 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/g' /etc/apt/apt.conf.d/20auto-upgrades
+sudo sed -i 's/#\$nrconf{verbosity} = '2';/\$nrconf{verbosity} = '0';/g' /etc/needrestart/needrestart.conf
+sudo sed -i 's/#\$nrconf{restart} = '\''i'\'';/\$nrconf{restart} = '\''a'\'';/g' /etc/needrestart/needrestart.conf
 sudo apt update -y && sudo apt upgrade -y
-#echo -e '\n'   # Enter
-#echo -e '\t'   # Tab
-#echo -e '\n'   # Enter
 sudo apt install openjdk-17-jre -y
-#echo -e '\n'   # Enter
-#echo -e '\t'   # Tab
-#echo -e '\n'   # Enter
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install jenkins -y
-#echo -e '\n'   # Enter
-#echo -e '\t'   # Tab
-#echo -e '\n'   # Enter
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
