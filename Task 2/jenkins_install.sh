5015d5cfb7b58c259e3d0a8d6ce0c8a1a2471d3c
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-28 12:09:39 +0600

    save

--- Task 2/jenkins_install.sh
+++ Task 2/jenkins_install.sh
@@ -2,11 +2,12 @@
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/g' /etc/apt/apt.conf.d/20auto-upgrades
 sudo sed -i 's/#\$nrconf{verbosity} = '2';/\$nrconf{verbosity} = '0';/g' /etc/needrestart/needrestart.conf
 sudo sed -i 's/#\$nrconf{restart} = '\''i'\'';/\$nrconf{restart} = '\''a'\'';/g' /etc/needrestart/needrestart.conf
-sudo apt update -y && sudo apt upgrade -y
+export DEBIAN_FRONTEND=noninteractive
+sudo apt update && sudo apt upgrade -y
 sudo apt install openjdk-17-jre -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
+sudo apt update
 sudo apt install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
