f24716f01f41e835dd2be919eb9a24f9fb56baca
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-29 17:36:33 +0600

    changed

--- Prerequisites/install_terraform.sh
+++ Prerequisites/install_terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install software-properties-common gnupg2 curl unzip wget -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install software-properties-common gnupg2 curl unzip wget -y
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+sudo apt-get install terraform
 terraform --version
--- Task 2/README.md
+++ Task 2/README.md
@@ -1,17 +1,3 @@
 ## Task 2
 
-I am going to follow 
-
-https://amlanscloud.com/terrakubedeploy/
-
-https://github.com/amlana21/terrakubepost-publish
-
-
-https://www.youtube.com/watch?v=9XrYwfIWDL0
-
-https://github.com/azeezsalu/terraform-tutorial-reference-files/blob/main/ec2-reference.tf
-
-https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/#installing-and-configuring-jenkins
-
-
-https://stackoverflow.com/questions/77893604/jenkins-installation-in-ubuntu-22-04-with-bash-script-using-in-terraform-gets-st
+The idea is to automate the deployment as much as possible.
