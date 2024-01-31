8ec5bf3def8b3c5b422f61eedf25050e82fb127b
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-01-31 14:28:31 +0600

    changes in the files

--- Task 2/1-vpc.tf
+++ Task 2/1-vpc.tf
@@ -1,10 +1,3 @@
-# configured aws provider with proper credentials
-provider "aws" {
-  #region    = "us-west-2"
-  profile   = "default"
-}
-
-
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
 
