984f0ea493cd13e7de6e749253099d78b155b427
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-02-11 15:02:22 +0600

    push

--- Task 2/install_jenkins.sh
+++ Task 2/install_jenkins.sh
@@ -4,7 +4,8 @@ sudo sed -i 's/#\$nrconf{verbosity} = '2';/\$nrconf{verbosity} = '0';/g' /etc/ne
 sudo sed -i 's/#\$nrconf{restart} = '\''i'\'';/\$nrconf{restart} = '\''a'\'';/g' /etc/needrestart/needrestart.conf
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install net-tools fontconfig openjdk-17-jre -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
